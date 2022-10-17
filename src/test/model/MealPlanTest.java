95b1694fdf5a63043e96ebf1f98bd7b6a1789ddb
karlie-tr <99240209+karlie-tr@users.noreply.github.com>
2022-10-17 06:52:20 -0700

    P1.T2: reformat code

--- src/test/model/MealPlanTest.java
+++ src/test/model/MealPlanTest.java
@@ -21,24 +21,23 @@ public class MealPlanTest {
     private Meal chickenBowl;
     private Meal bagel;
 
-
-
     @BeforeEach
     public void setUp() {
         overnightOatIngredientList.add("oat");
         overnightOatIngredientList.add("milk");
         overnightOatIngredientList.add("strawberries");
         overnightOatIngredientList.add("mango");
+
         chickenBowlIngredientList.add("rice");
         chickenBowlIngredientList.add("chicken");
         chickenBowlIngredientList.add("avocado");
+
         bagelAndCreamCheeseIngredientList.add("everything bagel");
         bagelAndCreamCheeseIngredientList.add("herb and garlic cream cheese");
 
         oats = new Meal("overnight oats",overnightOatIngredientList,10);
         chickenBowl = new Meal("chicken bowl",chickenBowlIngredientList,20);
         bagel = new Meal("bagel and cream cheese",bagelAndCreamCheeseIngredientList,5);
-
     }
 
     @Test
@@ -50,6 +49,9 @@ public class MealPlanTest {
         mp1.addNewMeal(chickenBowl);
         mealNames1.add(oats.getMealName());
         mealNames1.add(chickenBowl.getMealName());
+        assertEquals(2,mp1.getNumberOfMeals());
+        assertEquals(mealNames1,mp1.getNamesOfCurrentMeals());
+
         grocery1.add("oat");
         grocery1.add("milk");
         grocery1.add("strawberries");
@@ -57,11 +59,9 @@ public class MealPlanTest {
         grocery1.add("rice");
         grocery1.add("chicken");
         grocery1.add("avocado");
-
-        assertEquals(2,mp1.getNumberOfMeals());
-        assertEquals(mealNames1,mp1.getNamesOfCurrentMeals());
-        assertEquals(30,mp1.getTotalCookingTime());
         assertEquals(grocery1,mp1.getGroceryList());
+
+        assertEquals(30,mp1.getTotalCookingTime());
     }
 
     @Test
@@ -73,13 +73,14 @@ public class MealPlanTest {
         mp2.addNewMeal(bagel);
         mealNames2.add(bagel.getMealName());
         mealNames2.add(bagel.getMealName());
-        grocery2.add("everything bagel");
-        grocery2.add("herb and garlic cream cheese");
-
         assertEquals(2,mp2.getNumberOfMeals());
         assertEquals(mealNames2,mp2.getNamesOfCurrentMeals());
-        assertEquals(10,mp2.getTotalCookingTime());
+
+        grocery2.add("everything bagel");
+        grocery2.add("herb and garlic cream cheese");
         assertEquals(grocery2,mp2.getGroceryList());
+
+        assertEquals(10,mp2.getTotalCookingTime());
     }
 
     @Test
@@ -95,14 +96,15 @@ public class MealPlanTest {
         mp3.removeExistingMeal(oats);
         mealNames3.add(chickenBowl.getMealName());
         meals3.add(chickenBowl);
+        assertEquals(1,mp3.getNumberOfMeals());
+        assertEquals(mealNames3,mp3.getNamesOfCurrentMeals());
+        assertEquals(meals3,mp3.getMeals());
+
         grocery3.add("rice");
         grocery3.add("chicken");
         grocery3.add("avocado");
-
-        assertEquals(1,mp3.getNumberOfMeals());
-        assertEquals(mealNames3,mp3.getNamesOfCurrentMeals());
-        assertEquals(20,mp3.getTotalCookingTime());
-        assertEquals(meals3,mp3.getMeals());
         assertEquals(grocery3,mp3.getGroceryList());
+
+        assertEquals(20,mp3.getTotalCookingTime());
     }
 }
